7d804d3f39a614068fa7149061ff4581d8704d7d
benq <ben233q@gmail.com>
2016-10-06 19:54:33 +0800

    首页数据呈现，及UI路由

--- angularserver/www/js/controllers.js
+++ angularserver/www/js/controllers.js
@@ -1,103 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {
+.controller('HomeCtrl', function($scope,video,ad,sorts) {
 
 
-
-    $scope.zslesson = [
-
-        {
-            "videoId": 1,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.png",
-            "Ispriority": "",
-            "pro": "2/12"
-        }, {
-            "videoId": 12,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.png",
-            "Ispriority": "",
-            "pro": "2/12"
-        }, {
-            "videoId": 13,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.png",
-            "Ispriority": "",
-            "pro": "2/12"
-        }, {
-            "videoId": 14,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.png",
-            "Ispriority": "",
-            "pro": "2/12"
-        }
-
-    ];
-
-    $scope.relesson = [{
-        "videoId": 1,
-        "videoName": "大鱼海棠全网首播",
-        "videoPic": "images/banner1.jpg",
-        "Ispriority": "",
-        "pro": "2/12"
-    }, {
-        "videoId": 1,
-        "videoName": "大鱼海棠全网首播",
-        "videoPic": "images/banner1.jpg",
-        "Ispriority": "",
-        "pro": "2/12"
-    }, {
-        "videoId": 1,
-        "videoName": "大鱼海棠全网首播",
-        "videoPic": "images/banner1.jpg",
-        "Ispriority": "",
-        "pro": "2/12"
-    }, {
-        "videoId": 1,
-        "videoName": "大鱼海棠全网首播",
-        "videoPic": "images/banner1.jpg",
-        "Ispriority": "",
-        "pro": "2/12"
-    }, ];
-
-    $scope.sorts = [{
-        "id": 1,
-        "name": "全部",
-        "pic": "images/cicle1.png"
-    }, {
-        "id": 2,
-        "name": "语文",
-        "pic": "images/cicle2.png"
-    }, {
-        "id": 3,
-        "name": "数学",
-        "pic": "images/cicle3.png"
-    }, {
-        "id": 4,
-        "name": "映月",
-        "pic": "images/cicle4.png"
-    }, {
-        "id": 5,
-        "name": "映月",
-        "pic": "images/cicle4.png"
-    }, ];
-
-    $scope.ads = [
-
-        {
-            "adId": 1,
-            "adPic": "images/banner1.jpg"
-        }, {
-            "adId": 2,
-            "adPic": "images/banner2.jpg"
-        }, {
-            "adId": 3,
-            "adPic": "images/banner3.jpg"
-        }, {
-            "adId": 4,
-            "adPic": "images/banner4.jpg"
-        },
-    ]
+    $scope.zspriority = video.getpriority("zslesson");
+    $scope.repriority = video.getpriority("relesson");
+    $scope.zslesson = video.getTYPE("zslesson");
+    $scope.relesson = video.getTYPE("relesson");
+    $scope.sorts = sorts.all();
+    $scope.ads = ad.all();
 
     var Swiper1 = new Swiper('#swiper-container1', {
         direction: 'horizontal',
@@ -122,95 +33,13 @@ angular.module('starter.controllers', [])
 
 .controller('SearchCtrl', function($scope, $stateParams) {
 
-    $scope.video = [
-
-        {
-            "videoId": 1,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        }, {
-            "videoId": 2,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        }, {
-            "videoId": 3,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        }, {
-            "videoId": 4,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        }, {
-            "videoId": 5,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        }, {
-            "videoId": 6,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        }, {
-            "videoId": 7,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        }, {
-            "videoId": 8,
-            "videoName": "大鱼海棠全网首播",
-            "videoPic": "images/banner1.jpg",
-            "Ispriority": "",
-            "pro": "2/12",
-            "price": 0,
-            "ctime": "2016-07-02",
-            "status": 1,
-            "sign": 233
-        },
-    ]
+    $scope.video = video.all();
 
 })
 
-.controller('ContentCtrl', function($scope, $stateParams) {
+.controller('ContentCtrl', function($scope, $stateParams,video) {
 
-    $scope.video = $stateParams;
-    console.log($scope.video);
+    
+    $scope.video = video.getVideo($stateParams.videoId);
+    console.log(video.getVideo($stateParams.videoId));
 })
--- angularserver/www/js/services.js
+++ angularserver/www/js/services.js
@@ -1,50 +1,214 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('video', function() {
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    // Some fake testing data
+    var video = [
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        {
+            videoId: 1,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.png",
+            "Ispriority": 1,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "zslesson"
+        }, {
+            videoId: 12,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.png",
+            "Ispriority": 0,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "zslesson"
+        }, {
+            videoId: 13,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.png",
+            "Ispriority": 0,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "zslesson"
+        }, {
+            videoId: 14,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.png",
+            "Ispriority": 0,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "zslesson"
+        }, {
+            videoId: 14,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.jpg",
+            "Ispriority": 0,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "relesson"
+        }, {
+            videoId: 14,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.jpg",
+            "Ispriority": 0,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "relesson"
+        }, {
+            videoId: 14,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.jpg",
+            "Ispriority": 0,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "relesson"
+        }, {
+            videoId: 14,
+            "videoName": "大鱼海棠全网首播",
+            "videoPic": "images/banner1.jpg",
+            "Ispriority": 0,
+            "pro": "2/12",
+            "price": 0,
+            "ctime": "2016-07-02",
+            "status": 1,
+            "sign": 233,
+            type: "relesson"
         }
-      }
-      return null;
-    }
-  };
+
+    ];
+
+    return {
+        all: function() {
+            return video;
+        },
+        getVideo: function(videoId) {
+            for (var i = 0; i < video.length; i++) {
+                if (video[i].videoId === parseInt(videoId)) {
+                    return video[i];
+                }
+            }
+            return null;
+        },
+        getTYPE: function(type) {
+
+            var newVideo = [];
+            for (var i = 0; i < video.length; i++) {
+                if (video[i].type === type) {
+                    newVideo.push(video[i]);
+                }
+            }
+            return newVideo;
+
+        },
+
+        getpriority: function(type) {
+
+            var priority = null;
+            for (var i = 0; i < video.length; i++) {
+                if (video[i].type === type&&video[i].Ispriority==1) {
+
+                    return video[i];
+
+                }
+            }
+
+            return null;
+
+        }
+
+    };
+})
+
+
+.factory('ad', function() {
+
+    var ad = [
+
+        {
+            "adId": 1,
+            "adPic": "images/banner1.jpg"
+        }, {
+            "adId": 2,
+            "adPic": "images/banner2.jpg"
+        }, {
+            "adId": 3,
+            "adPic": "images/banner3.jpg"
+        }, {
+            "adId": 4,
+            "adPic": "images/banner4.jpg"
+        },
+    ];
+    return {
+        all: function() {
+            return ad;
+        },
+        getAd: function(adId) {
+            for (var i = 0; i < ad.length; i++) {
+                if (ad[i].adId === parseInt(adId)) {
+                    return ad[i];
+                }
+            }
+            return null;
+        },
+    };
+})
+
+
+.factory('sorts', function() {
+
+    var sorts = [{
+        "id": 1,
+        "name": "全部",
+        "pic": "images/cicle1.png"
+    }, {
+        "id": 2,
+        "name": "语文",
+        "pic": "images/cicle2.png"
+    }, {
+        "id": 3,
+        "name": "数学",
+        "pic": "images/cicle3.png"
+    }, {
+        "id": 4,
+        "name": "映月",
+        "pic": "images/cicle4.png"
+    }, {
+        "id": 5,
+        "name": "映月",
+        "pic": "images/cicle4.png"
+    }, ];
+    return {
+        all: function() {
+            return sorts;
+        },
+        getsorts: function(adId) {
+            for (var i = 0; i < sorts.length; i++) {
+                if (sorts[i].id === parseInt(id)) {
+                    return sorts[i];
+                }
+            }
+            return null;
+        },
+    };
 });
